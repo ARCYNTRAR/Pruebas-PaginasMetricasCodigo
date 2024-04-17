0059412e91597ceb504d1005e50ae9f8d50e27f3
ARCYNTRAR <arcangel557.ac@gmail.com>
2024-04-16 21:11:53 -0600

    Fizz Buzz Duplicado

---
+++ fizzBuzzDuplicado.js
@@ -0,0 +1,23 @@
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+  } else if (i % 3 === 0) {
+      console.log('Fizz');
+  } else if (i % 5 === 0) {
+      console.log('Buzz');
+  } else {
+      console.log(i);
+  }
+}
+
+for (let j = 1; j <= 100; j++) {
+  if (j % 3 === 0 && j % 5 === 0) {
+      console.log('FizzBuzz');
+  } else if (j % 3 === 0) {
+      console.log('Fizz');
+  } else if (j % 5 === 0) {
+      console.log('Buzz');
+  } else {
+      console.log(j);
+  }
+}
